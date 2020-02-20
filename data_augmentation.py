c0f8ef9d58837aca5eea7203018e6c3d490ffdaf
emilie0312 <59047001+emilie0312@users.noreply.github.com>
2020-02-20 21:49:48 +0100

    Add files via upload

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import numpy as np
 from specAugment import spec_augment_tensorflow
 # Time Shift Augmentation
 # spectrogram = time_shift_spectrogram(spectrogram)
+#时移增强是通过沿时间轴滚动信号来随机移位信号。包裹着移动。
 def time_shift_spectrogram(spectrogram):
     """ Shift a spectrogram along the time axis in the spectral-domain at random
     """
     nb_cols = spectrogram.shape[1]
     nb_shifts = np.random.randint(0, nb_cols)
-
     return np.roll(spectrogram, nb_shifts, axis=1)
 
 
 # Pitch Shift Augmentation
 # spectrogram = pitch_shift_spectrogram(spectrogram)
+#音高变化增量是围绕频率轴的±5％范围内的随机滚动。环绕式转换以保留所有信息。
 def pitch_shift_spectrogram(spectrogram):
     """ Shift a spectrogram along the frequency axis in the spectral-domain at
     random
